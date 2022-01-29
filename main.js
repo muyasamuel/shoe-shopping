2f467bbe5d8abb2ad9037219a6624827d37312f1
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-01-29 16:52:07 +0300

    js to add cart item on cart

--- main.js
+++ main.js
@@ -13,3 +13,39 @@ cartIcon.onclick = ()=> {
 closeCart.onclick = ()=> {
     cart.classList.remove("active");
 }
+
+//adding item to cart
+let addToCart = document.getElementsByClassName("add-cart");
+let cartContainer = document.getElementsByClassName("cart")[0];
+
+for( var i = 0; i < addToCart.length; i++){
+    addToCart[i].addEventListener("click" , addingToCart)
+}
+
+function addingToCart(){
+    let btn = event.target;
+    let btn_parent = btn.parentElement;
+    let itemName = btn_parent.children[1]
+    let itemPrice = btn_parent.children[2]
+    let itemImg = btn_parent.children[0].src
+    
+    let itemContainer = document.createElement("div");
+    itemContainer.innerHTML = `
+    <div class="cart-content">
+    <div class="cart-box">
+        <img src="${itemImg}" alt="" height="80px" width="80px" class="cart-img">
+        <div class="detail-box">
+            <div class="${itemName}">airmass2</div>
+            <div class="${itemPrice}">$45</div>
+            <input type="number" value="1" class="cart-quantity">
+            
+        </div>
+        <i class='bx bxs-trash-alt  cart-remove' ></i>
+
+    </div>`
+
+    cartContainer.append(itemContainer);
+    
+    
+    
+}
