ea054371b442f86f5e08f6b54398e2540446d275
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-01-29 16:32:44 +0300

    adding a js of bringing and taking off the cart-icon bar

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 
 
 
+const cartIcon = document.getElementById("cart-icon");
+const closeCart = document.getElementById("close-cart");
+const cart = document.querySelector(".cart");
+
+//bringing and taking off the cart-icon-bar
+
+cartIcon.onclick = ()=> {
+    cart.classList.add("active");
+}
+closeCart.onclick = ()=> {
+    cart.classList.remove("active");
+}
